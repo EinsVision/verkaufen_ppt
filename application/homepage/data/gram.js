9386c0ab8cc0ac805058735fb6efc24a34bf7fd5
jungchangdae <einsvision@gmail.com>
2021-01-24 22:56:41 +0900

    web page 만들기

---
+++ application/homepage/data/gram.js
@@ -0,0 +1,5 @@
+var name ='jcdlove';
+
+var letter = `Lorem ipsum dolor ${name} sit, amet consectetur adipisicing elit. Magni laudantium ullam temporibus, sunt reiciendis odit beatae, veniam vitae molestiae porro eius nam odio, ${1+1} aperiam esse exercitationem ${name} dignissimos a maiores eveniet!Lorem ipsum dolor sit, amet consectetur adipisicing elit. ${name} Magni laudantium ullam temporibus, ${1+1} sunt reiciendis odit beatae, veniam vitae ${name} molestiae porro eius nam odio, aperiam esse exercitationem dignissimos a maiores eveniet!`;
+
+console.log(letter);
